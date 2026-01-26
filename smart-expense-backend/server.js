8eed6bd7c1b31e526be7f1797412b422da25d148
Azlan Shah <146522695+CodeWithAzlo@users.noreply.github.com>
2026-01-26 23:49:15 +0500

    merge new changes

--- smart-expense-backend/server.js
+++ smart-expense-backend/server.js
@@ -1,12 +1,34 @@
-require('dotenv').config();
-const app = require('./app');
-const connectDB = require('./config/db');
-const initAdmin = require('./config/initAdmin'); 
+require("dotenv").config();
+const app = require("./app");
+const connectDB = require("./config/db");
+const initAdmin = require("./config/initAdmin");
 
 //  Database connection
-connectDB().then(() => {
-  initAdmin();
+// connectDB().then(() => {
+//   initAdmin();
+// });
+
+let isConnected = false;
+
+async function connectionToMongoDB() {
+  try {
+    await connectDB();
+    isConnected = true;
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error("Failed to connect to MongoDB:", error);
+  }
+}
+
+// add middleware to check and establish MongoDB connection
+app.use(async (req, res, next) => {
+  if (!isConnected) {
+    await connectionToMongoDB();
+  }
+  next();
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+module.exports = app;
